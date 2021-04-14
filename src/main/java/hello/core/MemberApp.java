662fb7fabf25c7f57f0bb1e68a5f8fb0ac63781f
dykim <dykim36@kinx.net>
2021-04-14 19:48:58 +0900

    관심사의 분리. AppConfig에서 의존관계 주입. DIP.

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -7,9 +7,13 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member memberA = new Member(1L, "memberA", Grade.VIP);
         memberService.join(memberA);
 
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new Member = " + memberA.getName());
+        System.out.println("find Member = " + findMember.getName());
     }
 }
